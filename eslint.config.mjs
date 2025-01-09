9c76e3eefb45d35e05031f5232f88bb77f43518c
Lwonbin <lwb9036@gmail.com>
2025-01-09 15:55:18 +0900

    feat: 프로필 구조변경 프로필 사진 선택추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,10 @@
 import airbnbBase from "eslint-config-airbnb-base";
 import airbnbBaseImports from "eslint-config-airbnb-base/rules/imports";
 import importPlugin from "eslint-plugin-import";
+
 export default [
   {
-    ignores: ["node_modules/**"],
+    ignores: ["node_modules/**"], // 무시할 파일/디렉토리 설정
   },
   {
     files: ["**/*.js"], // JS 파일에 대해 설정 적용
@@ -22,9 +23,14 @@ export default [
       reportUnusedDisableDirectives: true,
     },
     plugins: {
-      import: importPlugin, // eslint-plugin-import 추가
+      import: importPlugin, // eslint-plugin-import 플러그인 추가
     },
     rules: {
+      'no-console': 'warn', // console.log() 사용 시 경고
+      'no-var': 'error', // var 사용 금지
+      'prefer-const': 'warn', // const 사용 권장
+      'quotes': ['error', 'double'], // 따옴표는 항상 double quote
+      'semi': ['error', 'always'], // 세미콜론 항상 필요
       ...airbnbBase.rules,
       ...airbnbBaseImports.rules,
     },
