b7793eb78628d9041a3d075b6e61015e66ea6ee4
AlmondBreez3 <bee1162@naver.com>
2025-01-16 08:40:12 +0900

    feat: 모달 기본 세팅

--- src/components/modal-section/Modal.jsx
+++ src/components/modal-section/Modal.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-const Modal = () => {
+const Modal = ({ children }) => {
   return (
-    <div className="modal">
-      <div className="modal_popup">
-        <button className="close_btn" id="circle"></button>
-        {/* <iframe
-          src="profile.html"
-          style="width: 90vw; height: 90vh; border: none;"
-        ></iframe> */}
+    <>
+      <div
+        data-cy="modal-backdrop"
+        className="fixed top-0 left-0 w-full h-full backdrop-blur-md z-1"
+      ></div>
+
+      <div className="fixed z-10 w-1/2 p-8 m-0 transform -translate-x-1/2 -translate-y-1/2 border-none rounded shadow-xl top-1/2 left-1/2 bg-slate-500">
+        {children}
       </div>
-    </div>
+    </>
   );
 };
 
