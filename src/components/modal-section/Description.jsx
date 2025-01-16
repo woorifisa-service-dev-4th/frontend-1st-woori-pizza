2b8032b6cc6e093eee3ae7f909ca95fb1174c3ac
Lwonbin <lwb9036@gmail.com>
2025-01-16 14:23:39 +0900

    feat: add tailwind style-description

--- src/components/modal-section/Description.jsx
+++ src/components/modal-section/Description.jsx
@@ -2,14 +2,14 @@ import React from "react";
 const Description = ({ text, setText }) => {
 
     const onChange = (e) => {
-        setText(e.target.value); // Update the description text
+        setText(e.target.value);
     };
 
     return (
         <div className="description-container">
             <textarea
+                className="h-full w-full p-2 bg-gray-200 text-gray-900 border-2 border-black rounded-lg outline-none shadow-none"
                 id="description"
-                className="description-input"
                 onChange={onChange}
                 value={text}
                 placeholder="상세설명을 입력하세요"
