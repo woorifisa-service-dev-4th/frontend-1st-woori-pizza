11d2b0de368969f0d8a03241844f58bd5d26de6b
이한비 <99820610+AlmondBreez3@users.noreply.github.com>
2025-01-16 12:50:00 +0900

    feat: 상세 설명 저장 기능구현 (#57)

--- src/components/modal-section/Modal.jsx
+++ src/components/modal-section/Modal.jsx
@@ -1,25 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { NameArrContext } from "../../contexts/NameArrContext";
 import Description from "./Description";
 
 const Modal = () => {
   const context = useContext(NameArrContext);
+  const { selectedUser, updateDescription } = context;
+
+  const [description, setDescription] = useState(
+    selectedUser?.description || ""
+  ); // description 상태
+
+  // selectedUser가 변경되면 description도 업데이트
+  useEffect(() => {
+    if (selectedUser) {
+      setDescription(selectedUser.description);
+    }
+  }, [selectedUser]);
+
+  const handleSave = () => {
+    if (selectedUser) {
+      updateDescription(selectedUser.id, description);
+      // description을 업데이트
+      context.setModal(false); // 모달 닫기
+    }
+  };
+
   return (
-    <>
-      <div
-        className={`fixed inset-0 bg-black bg-opacity-50 ${
-          context.modal ? "block" : "hidden"
-        }`}
-      >
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] h-[400px] p-6 bg-white rounded-2xl">
-          <button
-            className="absolute top-2 right-2 w-[1.5vw] h-[1.5vw] rounded-full bg-red-500"
-            onClick={() => context.setModal(false)}
-          ></button>
-          <Description />
-        </div>
+    <div
+      className={`fixed inset-0 bg-black bg-opacity-50 ${
+        context.modal ? "block" : "hidden"
+      }`}
+    >
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[300px] h-[400px] p-6 bg-white rounded-2xl">
+        <button
+          className="absolute top-2 right-2 w-[1.5vw] h-[1.5vw] rounded-full bg-red-500"
+          onClick={() => context.setModal(false)}
+        ></button>
+        <Description text={description} setText={setDescription} />
+        <button
+          onClick={handleSave}
+          className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 mt-4 mb-4"
+        >
+          저장
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/seat-section/Seat.jsx
+++ src/components/seat-section/Seat.jsx
@@ -4,6 +4,16 @@ import { NameArrContext } from "../../contexts/NameArrContext";
 
 const Seat = ({ name }) => {
   const context = useContext(NameArrContext);
+
+  const handleSeatClick = () => {
+    const selectedUser = context.nameArr
+      .flat() // 2D 배열을 1D 배열로 변환
+      .find((user) => user.name === name); // 클릭된 name에 해당하는 유저 찾기
+    if (selectedUser) {
+      context.setSelectedUser(selectedUser); // selectedUser 설정
+      context.setModal(true); // 모달 열기
+    }
+  };
   return (
     <>
       <div className="seat" id={name}>
@@ -11,9 +21,7 @@ const Seat = ({ name }) => {
           className="pizza_box"
           src={imgLogo}
           alt="closed pizza box"
-          onClick={() => {
-            context.setModal(true);
-          }}
+          onClick={handleSeatClick}
         />
         <div className="name_area">
           <div className="name_tag">{name}</div>
